6c4fbe2726979391c03cec5494bee8c58ec568f4
Filip Filmar <filmil@gmail.com>
2026-05-03 01:55:49 +0000

    3: still broken, new approach

--- internal/cosim/vpi_wrapper.cpp
+++ internal/cosim/vpi_wrapper.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <stdint.h>
 
 std::unordered_map<int, InstanceState> instances;
 
@@ -18,20 +19,7 @@ void log_to_file(const char* msg) {
     fflush(stderr);
 }
 
-PLI_INT32 verilator_step_call(char *user_data) {
-    log_to_file("[VPI] verilator_step_call executed!\n");
-
-    vpiHandle systfref = vpi_handle(vpiSysTfCall, nullptr);
-    vpiHandle arg_iter = vpi_iterate(vpiArgument, systfref);
-    if (!arg_iter) return 0;
-
-    vpiHandle arg = vpi_scan(arg_iter);
-    s_vpi_value val;
-    val.format = vpiIntVal;
-    vpi_get_value(arg, &val);
-    int id = val.value.integer;
-    vpi_free_object(arg_iter);
-
+void verilator_step_call(int32_t id) {
     if (instances.find(id) == instances.end()) {
         std::string path_prefix = "";
         vpiHandle callH = vpi_handle(vpiSysTfCall, nullptr);
@@ -51,52 +39,19 @@ PLI_INT32 verilator_step_call(char *user_data) {
 
     InstanceState& state = instances[id];
 
-    char buf[256];
-    snprintf(buf, sizeof(buf), "[VPI] Syncing inputs for id %d\n", id);
-    log_to_file(buf);
-
     sync_inputs(state);
     eval_model(state);
     sync_outputs(state);
-
-    return 0;
 }
 
-PLI_INT32 cleanup_callback(p_cb_data cb_data) {
+void cleanup_callback() {
     log_to_file("[VPI] Cleanup callback executed!\n");
     instances.clear();
-    return 0;
 }
 
-void register_callbacks() {
-    log_to_file("[VPI] register_callbacks executed!\n");
-
-    s_cb_data cb_data;
-    cb_data.reason = cbEndOfSimulation;
-    cb_data.cb_rtn = cleanup_callback;
-    cb_data.obj = nullptr;
-    cb_data.time = nullptr;
-    cb_data.value = nullptr;
-    vpi_register_cb(&cb_data);
-
-    s_vpi_systf_data tf_data;
-    tf_data.type = vpiSysTask;
-    tf_data.sysfunctype = 0;
-    tf_data.tfname = "$verilator_step_call";
-    tf_data.calltf = verilator_step_call;
-    tf_data.compiletf = nullptr;
-    tf_data.sizetf = nullptr;
-    tf_data.user_data = nullptr;
-    vpi_register_systf(&tf_data);
-}
-
-} // Close the previous extern "C" block
-
-// Make sure it is exported cleanly
-extern "C" {
-void (*vlog_startup_routines[])(void) = {
-    register_callbacks,
+void (*vhpi_startup_routines[])(void) = {
     nullptr
 };
-}
+
+} // extern "C"
 
